cc99a405de5148d14b4d4a647288923870e70124
nrodage21 <96766500+1312namrata@users.noreply.github.com>
2025-07-21 22:25:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Flask!"
+    return "Hello, World from GitHub!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
+
 
